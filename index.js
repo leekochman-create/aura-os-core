fceb98ee9363fc5ad1e6d27ff33703c3acc4177e
leekochman-create <leekochman@gmail.com>
2025-12-10 10:33:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import express from "express";
 import cors from "cors";
-
 import createTwinRoute from "./routes/createTwin.js";
 import getTwinRoute from "./routes/getTwin.js";
-import uploadMediaRoute from "./routes/uploadMedia.js";
 
 const app = express();
 
@@ -12,11 +10,10 @@ app.use(express.json());
 
 app.use("/create_twin", createTwinRoute);
 app.use("/get_twin", getTwinRoute);
-app.use("/upload", uploadMediaRoute);
 
 app.get("/", (req, res) => {
-  res.send("AURA OS CORE API IS RUNNING ✔");
+  res.send("AURA OS CORE API RUNNING");
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log("Server running on " + PORT));
